c69d0a80b5605fe0c5390db2b6e53e2f2a292b3e
veselin valkanov <veselin.d.valkanov@gmail.com>
2024-01-28 11:58:43 +0200

    set up clean archetecture project

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = typeof(DependencyInjection).Assembly;
+        services.AddMediatR(configuration => configuration.RegisterServicesFromAssemblies(assembly));
+
+        services.AddValidatorsFromAssemblies((IEnumerable<System.Reflection.Assembly>)assembly);
+
+        return services;
+    }
+
+}
